c13a23ba5aebe281c6dcb46243eda0f63e8f63c9
Bibekpoudel43 <bibekpoudel43@gmail.com>
2018-05-20 01:25:31 +0545

    new form

---
+++ Assignment2/UpdateAdminProfile.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Assignment2
+{
+    partial class UpdateAdminProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNewPassword = new System.Windows.Forms.Label();
+            this.labeluname = new System.Windows.Forms.Label();
+            this.labelemail = new System.Windows.Forms.Label();
+            this.labelfname = new System.Windows.Forms.Label();
+            this.txtNewPassword = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.txtuname = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtfname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelOldPassword = new System.Windows.Forms.Label();
+            this.txtOldPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelNewPassword
+            // 
+            this.labelNewPassword.AutoSize = true;
+            this.labelNewPassword.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNewPassword.ForeColor = System.Drawing.Color.Tomato;
+            this.labelNewPassword.Location = new System.Drawing.Point(369, 345);
+            this.labelNewPassword.Name = "labelNewPassword";
+            this.labelNewPassword.Size = new System.Drawing.Size(0, 16);
+            this.labelNewPassword.TabIndex = 36;
+            // 
+            // labeluname
+            // 
+            this.labeluname.AutoSize = true;
+            this.labeluname.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeluname.ForeColor = System.Drawing.Color.Tomato;
+            this.labeluname.Location = new System.Drawing.Point(369, 228);
+            this.labeluname.Name = "labeluname";
+            this.labeluname.Size = new System.Drawing.Size(0, 16);
+            this.labeluname.TabIndex = 35;
+            // 
+            // labelemail
+            // 
+            this.labelemail.AutoSize = true;
+            this.labelemail.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelemail.ForeColor = System.Drawing.Color.Tomato;
+            this.labelemail.Location = new System.Drawing.Point(369, 173);
+            this.labelemail.Name = "labelemail";
+            this.labelemail.Size = new System.Drawing.Size(0, 16);
+            this.labelemail.TabIndex = 34;
+            // 
+            // labelfname
+            // 
+            this.labelfname.AutoSize = true;
+            this.labelfname.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelfname.ForeColor = System.Drawing.Color.Tomato;
+            this.labelfname.Location = new System.Drawing.Point(369, 123);
+            this.labelfname.Name = "labelfname";
+            this.labelfname.Size = new System.Drawing.Size(0, 16);
+            this.labelfname.TabIndex = 33;
+            // 
+            // txtNewPassword
+            // 
+            this.txtNewPassword.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtNewPassword.Location = new System.Drawing.Point(356, 319);
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.PasswordChar = '*';
+            this.txtNewPassword.Size = new System.Drawing.Size(219, 23);
+            this.txtNewPassword.TabIndex = 32;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Tomato;
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label8.Location = new System.Drawing.Point(225, 316);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(122, 21);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "New Password";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.BackColor = System.Drawing.Color.Tomato;
+            this.buttonUpdate.FlatAppearance.BorderColor = System.Drawing.Color.Tomato;
+            this.buttonUpdate.FlatAppearance.BorderSize = 0;
+            this.buttonUpdate.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightCoral;
+            this.buttonUpdate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            this.buttonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUpdate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdate.ForeColor = System.Drawing.Color.White;
+            this.buttonUpdate.Location = new System.Drawing.Point(298, 386);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(230, 32);
+            this.buttonUpdate.TabIndex = 29;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.clickUpdate);
+            // 
+            // txtuname
+            // 
+            this.txtuname.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtuname.Location = new System.Drawing.Point(356, 202);
+            this.txtuname.Name = "txtuname";
+            this.txtuname.Size = new System.Drawing.Size(219, 23);
+            this.txtuname.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Tomato;
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label6.Location = new System.Drawing.Point(225, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 21);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Username";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(356, 147);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(219, 23);
+            this.txtemail.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Tomato;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label4.Location = new System.Drawing.Point(226, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 21);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Email";
+            // 
+            // txtfname
+            // 
+            this.txtfname.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfname.Location = new System.Drawing.Point(356, 97);
+            this.txtfname.Name = "txtfname";
+            this.txtfname.Size = new System.Drawing.Size(219, 23);
+            this.txtfname.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Tomato;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label2.Location = new System.Drawing.Point(225, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 21);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Full Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Tomato;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(225, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Update Profile";
+            // 
+            // labelOldPassword
+            // 
+            this.labelOldPassword.AutoSize = true;
+            this.labelOldPassword.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOldPassword.ForeColor = System.Drawing.Color.Tomato;
+            this.labelOldPassword.Location = new System.Drawing.Point(369, 285);
+            this.labelOldPassword.Name = "labelOldPassword";
+            this.labelOldPassword.Size = new System.Drawing.Size(0, 16);
+            this.labelOldPassword.TabIndex = 39;
+            // 
+            // txtOldPassword
+            // 
+            this.txtOldPassword.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtOldPassword.Location = new System.Drawing.Point(356, 259);
+            this.txtOldPassword.Name = "txtOldPassword";
+            this.txtOldPassword.PasswordChar = '*';
+            this.txtOldPassword.Size = new System.Drawing.Size(219, 23);
+            this.txtOldPassword.TabIndex = 38;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Tomato;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label5.Location = new System.Drawing.Point(225, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 21);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Old Password";
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelID.ForeColor = System.Drawing.Color.Tomato;
+            this.labelID.Location = new System.Drawing.Point(413, 55);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(28, 16);
+            this.labelID.TabIndex = 40;
+            this.labelID.Text = "ID : ";
+            // 
+            // UpdateAdminProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 447);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.labelOldPassword);
+            this.Controls.Add(this.txtOldPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelNewPassword);
+            this.Controls.Add(this.labeluname);
+            this.Controls.Add(this.labelemail);
+            this.Controls.Add(this.labelfname);
+            this.Controls.Add(this.txtNewPassword);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.txtuname);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtfname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UpdateAdminProfile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "UpdateAdminProfile";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNewPassword;
+        private System.Windows.Forms.Label labeluname;
+        private System.Windows.Forms.Label labelemail;
+        private System.Windows.Forms.Label labelfname;
+        private System.Windows.Forms.TextBox txtNewPassword;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.TextBox txtuname;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtfname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelOldPassword;
+        private System.Windows.Forms.TextBox txtOldPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelID;
+    }
+}
---
+++ Assignment2/UpdateAdminProfile.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace Assignment2
+{
+    public partial class UpdateAdminProfile : Form
+    {
+        MySqlConnection con = new MySqlConnection("datasource=localhost; port=3306; username=root; database=bugtrackingsys; password=; SslMode=none;");
+        MySqlDataAdapter ada;
+        DataTable dt;
+        MySqlCommand myCommand;
+        public UpdateAdminProfile()
+        {
+            InitializeComponent();
+            labelID.Text = Convert.ToString(LoginForm.uId);
+            viewProfile();
+        }
+
+        private void clickUpdate(object sender, EventArgs e)
+        {
+            //validation
+            if (txtfname.Text == "")
+            {
+                labelfname.Text = "•Please enter your full name ";
+            }
+            else if (txtemail.Text == "")
+            {
+                labelemail.Text = " •Please enter your email ";
+            }
+            else if (txtuname.Text == "")
+            {
+                labeluname.Text = " •Please enter a username ";
+            }
+            else if (txtOldPassword.Text == "")
+            {
+                labelOldPassword.Text = "•Please enter your old password ";
+            }
+            else if (txtNewPassword.Text == "")
+            {
+                labelNewPassword.Text = "•Please enter a new password ";
+            }
+            else
+            {
+                updateProfile();
+            }
+        }
+
+        //load admin profile on the textbox
+        public void viewProfile() {
+            try
+            {
+                string query = "select * from users where id =" + labelID.Text;
+                dt = new DataTable();
+                ada = new MySqlDataAdapter(query, con);
+                ada.Fill(dt);
+
+                IDataReader uRes = dt.CreateDataReader();
+
+                if (dt.Rows.Count == 1)
+                {
+                    //Read the data and store them in the list
+                    while (uRes.Read())
+                    {
+                        txtfname.Text = uRes.GetString(1);
+                        txtemail.Text = uRes.GetString(2);
+                        txtuname.Text = uRes.GetString(3);
+                        txtOldPassword.Text = uRes.GetString(4);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error in a database connection", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public void updateProfile()
+        {
+            try
+            {
+                string myQuery = "UPDATE users set full_name='"+txtfname.Text+"', email='"+txtemail.Text+"', username='"+txtuname.Text+"', password='"+txtNewPassword.Text+"' WHERE id="+labelID.Text ;
+                myCommand = new MySqlCommand(myQuery, con);
+                con.Open();
+                if (myCommand.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Sucessfully updated!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error in a database connection", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            myCommand.Connection.Close();
+        }
+    }
+}
