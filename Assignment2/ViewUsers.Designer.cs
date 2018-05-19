5acc400e088a60857adf501d14b3d939ea3e28f2
Bibekpoudel43 <bibekpoudel43@gmail.com>
2018-05-20 01:26:09 +0545

    new form for viewing all user

---
+++ Assignment2/ViewUsers.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Assignment2
+{
+    partial class ViewUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewUsers = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnView = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewUsers
+            // 
+            this.dataGridViewUsers.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridViewUsers.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUsers.Location = new System.Drawing.Point(86, 48);
+            this.dataGridViewUsers.Name = "dataGridViewUsers";
+            this.dataGridViewUsers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridViewUsers.Size = new System.Drawing.Size(726, 340);
+            this.dataGridViewUsers.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Tomato;
+            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.Color.Tomato;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightCoral;
+            this.btnDelete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(567, 9);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(81, 24);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.clickDelete);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Tomato;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(81, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Users";
+            // 
+            // btnView
+            // 
+            this.btnView.BackColor = System.Drawing.Color.Tomato;
+            this.btnView.FlatAppearance.BorderColor = System.Drawing.Color.Tomato;
+            this.btnView.FlatAppearance.BorderSize = 0;
+            this.btnView.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightCoral;
+            this.btnView.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            this.btnView.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnView.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnView.ForeColor = System.Drawing.Color.White;
+            this.btnView.Location = new System.Drawing.Point(456, 9);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(94, 24);
+            this.btnView.TabIndex = 8;
+            this.btnView.Text = "Show  Users";
+            this.btnView.UseVisualStyleBackColor = false;
+            this.btnView.Click += new System.EventHandler(this.clickView);
+            // 
+            // ViewUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 446);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dataGridViewUsers);
+            this.Name = "ViewUsers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ViewUsers";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.ViewUsers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewUsers;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnView;
+    }
+}
---
+++ Assignment2/ViewUsers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Assignment2
+{
+    public partial class ViewUsers : Form
+    {
+        MySqlConnection con = new MySqlConnection("datasource=localhost; port=3306; username=root; database=bugtrackingsys; password=; SslMode=none;");
+        MySqlDataAdapter ada;
+        DataSet dt;
+        public ViewUsers()
+        {
+            InitializeComponent();
+        }
+
+        private void clickView(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "select * from users u, roles r, user_roles ur where u.id = ur.user_id and r.id = ur.role_id";
+                ada = new MySqlDataAdapter(query, con);
+                con.Open();
+                dt = new DataSet();
+                ada.Fill(dt, "users");
+                dataGridViewUsers.DataSource = dt.Tables["users"];
+                dataGridViewUsers.Columns["password"].Visible = false;
+                dataGridViewUsers.Columns["id1"].Visible = false;
+                dataGridViewUsers.Columns["id2"].Visible = false;
+                dataGridViewUsers.Columns["user_id"].Visible = false;
+                dataGridViewUsers.Columns["role_id"].Visible = false;
+                con.Close();
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void ViewUsers_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clickDelete(object sender, EventArgs e)
+        {
+           try {
+                string selected_uid = dataGridViewUsers.CurrentRow.Cells["id"].Value.ToString();
+                string query = "DELETE from users where id=" +int.Parse(selected_uid);
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                if(cmd.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("Sucessfully deleted!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Something's wrong", "Info", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                con.Close();
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show("Error in db connection" + x.Message, "Info", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+   
+        }
+    }
+}
