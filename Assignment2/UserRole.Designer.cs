7fd29db15166a13be794fad59bc5c5ac7129b426
Bibekpoudel43 <bibekpoudel43@gmail.com>
2018-05-19 12:51:29 +0545

    for user roles - added by admin

---
+++ Assignment2/UserRole.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Assignment2
+{
+    partial class UserRole
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUser = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxRole = new System.Windows.Forms.ComboBox();
+            this.labelRole = new System.Windows.Forms.Label();
+            this.comboBoxUser = new System.Windows.Forms.ComboBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUser.ForeColor = System.Drawing.Color.Tomato;
+            this.labelUser.Location = new System.Drawing.Point(364, 135);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(0, 16);
+            this.labelUser.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Tomato;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label2.Location = new System.Drawing.Point(258, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 21);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "User";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Tomato;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(219, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 25);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Add Role For User";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Tomato;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label3.Location = new System.Drawing.Point(258, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 21);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Role";
+            // 
+            // comboBoxRole
+            // 
+            this.comboBoxRole.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxRole.FormattingEnabled = true;
+            this.comboBoxRole.Location = new System.Drawing.Point(351, 175);
+            this.comboBoxRole.Name = "comboBoxRole";
+            this.comboBoxRole.Size = new System.Drawing.Size(219, 24);
+            this.comboBoxRole.TabIndex = 22;
+            // 
+            // labelRole
+            // 
+            this.labelRole.AutoSize = true;
+            this.labelRole.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRole.ForeColor = System.Drawing.Color.Tomato;
+            this.labelRole.Location = new System.Drawing.Point(364, 197);
+            this.labelRole.Name = "labelRole";
+            this.labelRole.Size = new System.Drawing.Size(0, 16);
+            this.labelRole.TabIndex = 23;
+            // 
+            // comboBoxUser
+            // 
+            this.comboBoxUser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxUser.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxUser.FormattingEnabled = true;
+            this.comboBoxUser.Location = new System.Drawing.Point(351, 111);
+            this.comboBoxUser.Name = "comboBoxUser";
+            this.comboBoxUser.Size = new System.Drawing.Size(219, 25);
+            this.comboBoxUser.TabIndex = 24;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.Tomato;
+            this.buttonAdd.FlatAppearance.BorderColor = System.Drawing.Color.Tomato;
+            this.buttonAdd.FlatAppearance.BorderSize = 0;
+            this.buttonAdd.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightCoral;
+            this.buttonAdd.FlatAppearance.MouseOverBackColor = System.Drawing.Color.OrangeRed;
+            this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAdd.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.ForeColor = System.Drawing.Color.White;
+            this.buttonAdd.Location = new System.Drawing.Point(351, 251);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(130, 32);
+            this.buttonAdd.TabIndex = 25;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.clickAdd);
+            // 
+            // UserRole
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(772, 453);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.comboBoxUser);
+            this.Controls.Add(this.labelRole);
+            this.Controls.Add(this.comboBoxRole);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UserRole";
+            this.Text = "UserRole";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxRole;
+        private System.Windows.Forms.Label labelRole;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxUser;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
---
+++ Assignment2/UserRole.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Assignment2
+{
+    public partial class UserRole : Form
+    {
+        MySqlConnection con = new MySqlConnection("datasource=localhost; port=3306; username=root; database=bugtrackingsys; password=; SslMode=none;");
+        MySqlDataAdapter ada;
+        DataSet dt;
+
+        public UserRole()
+        {
+            InitializeComponent();
+            selectRole();
+            selectUsers();
+        }
+
+        private void clickAdd(object sender, EventArgs e)
+        {
+            //validation
+            if (comboBoxRole.Text == "")
+            {
+                labelRole.Text = "•Please Select Any Role";
+            }
+            else if (comboBoxUser.Text == "")
+            {
+                labelUser.Text = " •Please Select Any  User";
+            }
+        }
+
+        //fecth data from database and load it into a combo box
+        public void selectRole()
+        {
+            var list = new List<string>();
+            dt = new DataSet();
+            try
+            {
+                string query = "select id, user_type from roles";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                ada = new MySqlDataAdapter(query, con);
+                ada.Fill(dt);
+                comboBoxRole.DataSource = dt.Tables[0];
+                comboBoxRole.DisplayMember = "user_type";
+                comboBoxRole.ValueMember = "id";
+                comboBoxRole.Enabled = true;
+                comboBoxRole.SelectedIndex = -1;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error in a database connection", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+       }
+
+
+        //fecth data from database and load it into a combo box
+        public void selectUsers()
+        {
+            var list = new List<string>();
+            dt = new DataSet();
+            try
+            {
+                string query = "select id, username from users";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                ada = new MySqlDataAdapter(query, con);
+                ada.Fill(dt);
+                comboBoxUser.DataSource = dt.Tables[0];
+                comboBoxUser.DisplayMember = "username";
+                comboBoxUser.ValueMember = "id";
+                comboBoxUser.Enabled = true;
+                comboBoxUser.SelectedIndex = -1;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error in a database connection", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+ }
+
