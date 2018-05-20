2a0f4f557eabb7dd61bd8c32a9fd4bb5bf2e267c
Bibekpoudel43 <bibekpoudel43@gmail.com>
2018-05-20 09:46:51 +0545

    new view code form

---
+++ Assignment2/ViewCode.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Assignment2
+{
+    partial class ViewCode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fastColoredTextBoxCode = new FastColoredTextBoxNS.FastColoredTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBoxCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fastColoredTextBoxCode
+            // 
+            this.fastColoredTextBoxCode.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBoxCode.AutoScrollMinSize = new System.Drawing.Size(27, 14);
+            this.fastColoredTextBoxCode.BackBrush = null;
+            this.fastColoredTextBoxCode.CharHeight = 14;
+            this.fastColoredTextBoxCode.CharWidth = 8;
+            this.fastColoredTextBoxCode.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBoxCode.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBoxCode.IsReplaceMode = false;
+            this.fastColoredTextBoxCode.Location = new System.Drawing.Point(-2, 1);
+            this.fastColoredTextBoxCode.Name = "fastColoredTextBoxCode";
+            this.fastColoredTextBoxCode.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBoxCode.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBoxCode.Size = new System.Drawing.Size(573, 424);
+            this.fastColoredTextBoxCode.TabIndex = 0;
+            this.fastColoredTextBoxCode.Zoom = 100;
+            this.fastColoredTextBoxCode.Load += new System.EventHandler(this.fastColoredTextBoxCode_Load);
+            // 
+            // ViewCode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 426);
+            this.Controls.Add(this.fastColoredTextBoxCode);
+            this.Name = "ViewCode";
+            this.Text = "ViewCode";
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBoxCode)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBoxCode;
+    }
+}
---
+++ Assignment2/ViewCode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment2
+{
+    public partial class ViewCode : Form
+    {
+        public ViewCode()
+        {
+            InitializeComponent();
+        }
+
+        private void fastColoredTextBoxCode_Load(object sender, EventArgs e)
+        { 
+
+            if(ViewBug.Language == "C#")
+            {
+                fastColoredTextBoxCode.Language = FastColoredTextBoxNS.Language.CSharp;
+                fastColoredTextBoxCode.Text = ViewBug.code;
+            }
+            else if (ViewBug.Language == "HTML")
+            {
+                fastColoredTextBoxCode.Language = FastColoredTextBoxNS.Language.HTML;
+                fastColoredTextBoxCode.Text = ViewBug.code;
+            }
+            else if (ViewBug.Language == "PHP")
+            {
+                fastColoredTextBoxCode.Language = FastColoredTextBoxNS.Language.PHP;
+                fastColoredTextBoxCode.Text = ViewBug.code;
+            }
+            else if (ViewBug.Language == "JS")
+            {
+                fastColoredTextBoxCode.Language = FastColoredTextBoxNS.Language.JS;
+                fastColoredTextBoxCode.Text = ViewBug.code;
+            }
+        }
+    }
+}
